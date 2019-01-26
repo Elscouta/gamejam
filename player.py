4383bc1bc55720b79d4dfd26eae7ead522da91df
Jonathan Jayet <jayet.j@gmail.com>
2019-01-26 12:30:52 +0100

    struff

--- player.py
+++ player.py
@@ -30,14 +30,19 @@ class Player(pg.sprite.Sprite):
         return not any(map.get_tile(x+dx, y+dy).is_collision(rel_x-dx, rel_y-dy)
                        for (dx, dy) in product({0, TILE_WIDTH}, {0, TILE_HEIGHT}))
 
-    def increment_cycle(self):
-        self.current_horizontal_cycle += 1
-        if self.current_horizontal_cycle == 3:
-            self.current_horizontal_cycle = 0
+    def increment_cycle(self, update_cycle: bool):
+        if update_cycle:
+            self.current_horizontal_cycle += 1
+            if self.current_horizontal_cycle == 3:
+                self.current_horizontal_cycle = 0
+            return False
+        return True
 
     def handle_keys(self):
         key = pg.key.get_pressed()
         dist = 1
+
+        update_cycle = True
 
         for _ in range(0, PLAYER_SPEED):
             new_x = self.x
@@ -46,19 +51,17 @@ class Player(pg.sprite.Sprite):
             if key[pg.K_DOWN]:
                 new_y = self.y + dist
                 self.image = self.sprites[0][self.current_horizontal_cycle]
-                self.increment_cycle()
             elif key[pg.K_UP]:
                 new_y = self.y - dist
                 self.image = self.sprites[3][self.current_horizontal_cycle]
-                self.increment_cycle()
             if key[pg.K_RIGHT]:
                 new_x = self.x + dist
                 self.image = self.sprites[2][self.current_horizontal_cycle]
-                self.increment_cycle()
             elif key[pg.K_LEFT]:
                 new_x = self.x - dist
                 self.image = self.sprites[1][self.current_horizontal_cycle]
-                self.increment_cycle()
+
+            update_cycle = self.increment_cycle(update_cycle)
 
             if self.valid_position(new_x, new_y):
                 self.x = new_x
