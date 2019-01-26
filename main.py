7c6de9e34c4562b2f6ee1c8c414f42acdab6ac6b
Jonathan Jayet <jayet.j@gmail.com>
2019-01-26 20:16:17 +0100

    struff

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-import pygame as pg
 import sys
 
+import pygame as pg
 from pygame.locals import *
 
-from config import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_SPEED
 import events
+from config import SCREEN_HEIGHT, SCREEN_WIDTH, PLAYER_SPEED
+from end_screen.end_screen import EndScreen
 from game_screen.game_screen import GameScreen
+from screen import Screen
+from title_screen.title_screen import TitleScreen
+
+
+def get_next_screen(current_displayed_screen: Screen) -> Screen:
+    if not current_displayed_screen:
+        return GameScreen()
+    if type(current_displayed_screen) is TitleScreen:
+        return GameScreen()
+    if type(current_displayed_screen) is GameScreen:
+        return EndScreen()
+
 
 pg.init()
+
+currentDisplayScreen: Screen = None
 
 screen: pg.Surface = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), FULLSCREEN)
 
@@ -16,8 +31,6 @@ pg.display.set_caption("Popo")
 events.init()
 
 clock = pg.time.Clock()
-
-currentScreen = GameScreen()
 
 while True:
     for event in pg.event.get():
@@ -30,5 +43,5 @@ while True:
 
     screen.fill((0, 0, 0))
 
-    if type(currentScreen) is GameScreen:
-        currentScreen.draw(screen, clock, PLAYER_SPEED)
+    if not currentDisplayScreen or currentDisplayScreen.draw(screen, clock, PLAYER_SPEED):
+        currentDisplayScreen = get_next_screen(currentDisplayScreen)
