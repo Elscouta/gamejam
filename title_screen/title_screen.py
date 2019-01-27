76a2127073cbbca5f2b3e87611a377b25fc49cfb
Rémi Bonnet <git@elscouta.net>
2019-01-27 03:06:25 +0100

    No more epilepsy.

--- title_screen/title_screen.py
+++ title_screen/title_screen.py
@@ -3,6 +3,7 @@ import os
 import pygame as pg
 from pygame.constants import *
 
+from events import schedule_event, clear_event
 from screen import Screen
 from random import randint
 
@@ -10,24 +11,27 @@ class TitleScreen(Screen):
     def __init__(self):
         self.font = pg.font.Font(os.path.join('assets', 'ButterflyKids-Regular.ttf'), 70)
         self.red = 255
-        self.green = 255
-        self.blue = 255
+        self.green = 215
+        self.blue = 185
+        self.update_event = schedule_event(self.update_colors, 1, oneshot=False)
 
     def update_colors(self):
-        self.red -= randint(0, 10)
+        self.red -= randint(0, 20)
         if self.red <= 5:
             self.red = 255
-        self.green -= randint(0, 10)
+        self.green -= randint(0, 20)
         if self.green <= 5:
             self.green = 255
-        self.blue -= randint(0, 10)
+        self.blue -= randint(0, 20)
         if self.blue <= 5:
             self.blue = 255
+
+    def cleanup(self):
+        clear_event(self.update_event)
 
     def draw(self, screen: pg.Surface, clock: pg.time.Clock, player_speed: int) -> bool:
         surface = self.font.render('A Loo In the Dark', False, (self.red, self.green, self.blue))
         text_rec = surface.get_rect(center=(screen.get_size()[0] // 2, screen.get_size()[1] // 2))
         screen.blit(surface, text_rec)
-        self.update_colors()
 
         return False
